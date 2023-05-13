400c02c418f0a47ded0969bb900dcb484367a09c
ryanjnam99 <ryanjnam@yahoo.com>
2023-05-12 21:36:00 -0700

    finished

---
+++ script.js
@@ -0,0 +1,11 @@
+function logout(element) {
+    element.innerText ="logout";
+}
+
+function likes(element) {
+    alert("Ninjas was liked.");
+}
+
+function blank(element) {
+    element.remove();
+}
